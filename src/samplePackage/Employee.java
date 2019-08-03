f36899782f2e08e66c1785bd9b2adfbcb8a1d164
anuththara kavindi <anukavindi@gmail.com>
2019-08-03 13:20:10 +0530

    employee class

---
+++ src/samplePackage/Employee.java
@@ -0,0 +1,21 @@
+package samplePackage;
+
+public class Employee {
+	private String empid;
+	private String empname;
+	public String getEmpid() {
+		return empid;
+	}
+	public void setEmpid(String empid) {
+		this.empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	
+	
+
+}
